f30a9db92eb736387f5f8e246d79096a5e3b91fe
Alberto Mardegan <mardy@users.sourceforge.net>
2026-02-13 17:49:05 +0300

    Test sbrk behaviour

--- source/main.c
+++ source/main.c
@@ -6,6 +6,45 @@
 static void *xfb = NULL;
 static GXRModeObj *rmode = NULL;
 
+extern int s_log_count;
+extern char s_log_buffer[1024];
+
+static void check_log() {
+    if (s_log_count > 0) {
+        puts(s_log_buffer);
+        s_log_count = 0;
+    }
+}
+
+static void mem_report()
+{
+    printf("MEM1: %p-%p, MEM2: %p-%p\n", SYS_GetArenaLo(), SYS_GetArenaHi(), SYS_GetArena2Lo(), SYS_GetArena2Hi());
+}
+
+static void *try_allocate(size_t size)
+{
+    void *ptr = malloc(size);
+    check_log();
+    if (ptr) {
+        printf("Allocating %d bytes succeeded: %p\n", size, ptr);
+    } else {
+        printf("Allocating %d bytes failed\n", size);
+    }
+    return ptr;
+}
+
+static void try_free(void *ptr)
+{
+    if (!ptr) {
+        puts("Asked to free a NULL pointer!\n");
+        return;
+    }
+
+    printf("Freeing %p\n", ptr);
+    free(ptr);
+    check_log();
+}
+
 //---------------------------------------------------------------------------------
 int main(int argc, char **argv) {
 //---------------------------------------------------------------------------------
@@ -47,11 +86,12 @@ int main(int argc, char **argv) {
 	// This positions the cursor on row 2, column 0
 	// we can use variables for this with format codes too
 	// e.g. printf ("\x1b[%d;%dH", row, column );
-	printf("\x1b[2;0H");
+	//printf("\x1b[2;0H");
 
 
 	printf("Hello World!");
 
+    int step = 0;
 	while(SYS_MainLoop()) {
 
 		// Call WPAD_ScanPads each loop, this reads the latest controller states
@@ -64,6 +104,26 @@ int main(int argc, char **argv) {
 		// We return to the launcher application via exit
 		if ( pressed & WPAD_BUTTON_HOME ) exit(0);
 
+        if (step == 30) {
+            check_log();
+            void *mem1_10mb = try_allocate(10*1024*1024);
+            /* This will get allocated in MEM2: */
+            void *mem2_20mb = try_allocate(20*1024*1024);
+            /* We would like to see this in MEM1, but alas */
+            void *mem2_5mb = try_allocate(5*1024*1024);
+            /* This will get allocated in MEM2: */
+            void *mem2_1mb = try_allocate(1*1024*1024);
+
+            try_free(mem2_20mb);
+            try_free(mem1_10mb);
+            mem_report();
+
+            try_free(mem2_1mb);
+            mem_report();
+            try_free(mem2_5mb);
+            mem_report();
+        }
+        step++;
 		// Wait for the next frame
 		VIDEO_WaitVSync();
 	}
